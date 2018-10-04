9cec8abaacee4315df6a8f5ef97da23bf3a4c93e
mmajdanski <michael.majdanski@gmail.com>
2018-10-03 18:13:25 -0700

    changes in sagas.

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -5,9 +5,11 @@ const initialState = 0;
 export const counterReducer = (state = initialState, action) => {
   switch (action.type) {
     case UPDATE_COUNTER:
+      console.log("something 1");
       return state;
 
     case UPDATE_COMPLETE:
+      console.log("something 2");
       return state + action.payload;
 
     default:
--- src/sagas/counter.js
+++ src/sagas/counter.js
@@ -1,3 +1,14 @@
-export function* helloSaga() {
-  console.log("Hello Sagas!");
+import { delay } from "redux-saga";
+import { put, takeEvery } from "redux-saga/effects";
+
+export function* count(payload) {
+  console.log(payload);
+  yield delay(1000);
+  yield put({ type: "UPDATE_COMPLETE", payload: 1 });
+  //Need to have payload sent here...
+}
+
+// Our watcher Saga: spawn a new incrementAsync task on each INCREMENT_ASYNC
+export function* watchCounter() {
+  yield takeEvery("UPDATE_COUNTER", count);
 }
