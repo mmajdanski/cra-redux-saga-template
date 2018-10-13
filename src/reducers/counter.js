2131d051a5f045643bf2babc61295aee4e124c51
mmajdanski <michael.majdanski@gmail.com>
2018-10-13 10:08:55 -0700

    saga and reducer fix

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -1,15 +1,10 @@
-import { UPDATE_COUNTER, UPDATE_COMPLETE } from "../actions/counterActions";
+import { UPDATE_COMPLETE } from "../actions/counterActions";
 
 const initialState = 0;
 
 export const counterReducer = (state = initialState, action) => {
   switch (action.type) {
-    case UPDATE_COUNTER:
-      console.log("something 1");
-      return state;
-
     case UPDATE_COMPLETE:
-      console.log("something 2");
       return state + action.payload;
 
     default:
--- src/sagas/counter.js
+++ src/sagas/counter.js
@@ -1,14 +1,13 @@
+import { UPDATE_COUNTER, UPDATE_COMPLETE } from "../actions/counterActions";
 import { delay } from "redux-saga";
 import { put, takeEvery } from "redux-saga/effects";
 
-export function* count(payload) {
-  console.log(payload);
-  yield delay(1000);
-  yield put({ type: "UPDATE_COMPLETE", payload: 1 });
-  //Need to have payload sent here...
+export function* count(action) {
+  yield delay(1000); //Delay the Counter By 1 Second
+  yield put({ type: UPDATE_COMPLETE, payload: action.payload }); //Send the Final Payload to the Reducer
 }
 
 // Our watcher Saga: spawn a new incrementAsync task on each INCREMENT_ASYNC
 export function* watchCounter() {
-  yield takeEvery("UPDATE_COUNTER", count);
+  yield takeEvery(UPDATE_COUNTER, count);
 }
